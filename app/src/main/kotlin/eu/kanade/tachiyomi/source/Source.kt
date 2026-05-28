38258041a2619c00d2bc411e21c7143697c3958c
HuzaifaKhalid1311 <huzaifa1311.0@gmail.com>
2026-05-28 23:30:12 +0500

    idk claude did it, sum crash prevention

--- app/src/main/kotlin/eu/kanade/tachiyomi/source/Source.kt
+++ app/src/main/kotlin/eu/kanade/tachiyomi/source/Source.kt
@@ -3,6 +3,7 @@ package eu.kanade.tachiyomi.source
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.model.SMangaUpdate
 import eu.kanade.tachiyomi.util.awaitSingle
 import rx.Observable
 
@@ -26,6 +27,21 @@ interface Source {
 	@Suppress("DEPRECATION")
 	suspend fun getPageList(chapter: SChapter): List<Page> {
 		return fetchPageList(chapter).awaitSingle()
+	}
+
+	/**
+	 * Combined manga + chapter fetch (extensions-lib 1.5+).
+	 * Host falls back to separate calls; sources may override for efficiency.
+	 */
+	suspend fun getMangaUpdate(
+		manga: SManga,
+		chapters: List<SChapter>,
+		fetchDetails: Boolean,
+		fetchChapters: Boolean,
+	): SMangaUpdate {
+		val updatedManga = if (fetchDetails) getMangaDetails(manga) else manga
+		val updatedChapters = if (fetchChapters) getChapterList(manga) else chapters
+		return SMangaUpdate(updatedManga, updatedChapters)
 	}
 
 	@Deprecated("Use the non-RxJava API instead", ReplaceWith("getMangaDetails"))
